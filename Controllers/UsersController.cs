2a8bce6ddcb5ca487052296bd7032ec424180ac3
penduko <clydebryan.ferrolino@gmail.com>
2018-05-07 21:01:06 +0800

    Update Profile Functions

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using AutoMapper;
 using DatingApp.API.Data;
@@ -36,6 +38,38 @@ namespace DatingApp.API.Controllers
             var userToReturn = _mapper.Map<UserForDetailedDto>(user);
 
             return Ok(userToReturn);      
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateUser(int id, [FromBody] UserForUpdateDto userForUpdateDto)
+        {
+            // checked if the modelstate if valid
+            if(!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            // get the id of the user that sent request to the api
+            var currentUserId = int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value);
+            
+            // get the user from repo
+            var userFromRepo = await _repo.GetUser(id);
+
+            if(userFromRepo == null)
+                return NotFound($"Could not find user of ID of {id}");
+            
+            // compare the currentuserid with userfromrepo id to make user it match
+            // so that the only current loggin user can update their profile
+            if(currentUserId != userFromRepo.Id)
+                return Unauthorized();
+
+            // map
+            _mapper.Map(userForUpdateDto, userFromRepo);
+
+            if (await _repo.SaveAll())
+                // success return no content with the status code of 204
+                return NoContent();
+
+            
+            throw new Exception($"Updating user {id} failed on save");
         }
     }
 }
